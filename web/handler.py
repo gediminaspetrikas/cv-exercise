853e99ea098836ca1e3267e433d6f4cc2580c343
Gediminas Petrikas <gediminas.petrikas@hostmaker.co>
2021-02-16 19:36:12 +0200

    feat: Restructure project into services and resources

--- web/handler.py
+++
@@ -1,8 +0,0 @@
-def hello(event, context):
-    return {
-        "statusCode": 200,
-        "body": "<html><body><p>Hello!</p></body></html>",
-        "headers": {
-            "Content-Type": "text/html"
-        }
-    }
