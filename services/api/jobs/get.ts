f9c7fce79f0c1b7521db70f1defca38ebce3a63b
Gediminas Petrikas <gediminas.petrikas@hostmaker.co>
2021-02-21 20:38:10 +0200

    chore: Log id

--- services/api/jobs/get.ts
+++ services/api/jobs/get.ts
@@ -7,7 +7,7 @@ const dynamoDb = new DynamoDB.DocumentClient();
 
 export const get: Handler = async (event) => {
   const id = event.pathParameters.id;
-  console.log(id);
+  console.log("ID: ", id);
   if (!id || uuid.validate(id)) {
     return {
       statusCode: 400,
@@ -24,7 +24,7 @@ export const get: Handler = async (event) => {
 
   try {
     const { Item: result } = await dynamoDb.get(params).promise();
-    console.log(result);
+    console.log("item: ", result);
 
     const mappedResult = {
       jobId: result.id,
